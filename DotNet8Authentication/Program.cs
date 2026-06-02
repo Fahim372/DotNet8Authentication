11bab23286b18ea19e8d089a9134ef1f52b1e915
Fahim <Fahim@dohatec.com.bd>
2026-06-02 16:01:02 +0600

    ok

--- DotNet8Authentication/Program.cs
+++ DotNet8Authentication/Program.cs
@@ -1,3 +1,6 @@
+using DotNet8Authentication.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+//    (options =>
+//{
+//    options.AddPolicy("AdminOnly", policy => policy.RequireClaim("Role", "Admin"));
+//    options.AddPolicy("UserOnly", policy => policy.RequireClaim("Role", "User"));
+//});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
